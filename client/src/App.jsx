d766a65debff5bca42ac741d3c7e849cd932e0d8
MATSUINAOKO <119477648+MATSUINAOKO@users.noreply.github.com>
2023-11-29 00:36:27 +0900

    ADD：履歴用コンポーネント追加しました
    Co-authored-by: Ogata-Kazuyoshi <ogata-kazuyoshi@users.noreply.github.com>
    Co-authored-by: MATSUINAOKO <matsuinaoko@users.noreply.github.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,9 @@ import Top from './component/Top'
 import AuthTop from './component/auth/AuthTop'
 import Login from './component/auth/Login'
 import SignUp from './component/auth/SignUp'
-import PatientTop from "./component/patient/PatientTop"
+import Header from "./component/common/Header"
 import History from "./component/common/History"
 import InputPatient from "./component/patient/InputPatient"
-import DoctorTop from "./component/doctor/DoctorTop"
 import InputDoctor from "./component/doctor/InputDoctor"
 
 
@@ -23,12 +22,12 @@ function App() {
         <Route path="/auth/login" element={<Login />} />
         <Route path="/auth/signup" element={<SignUp />} />
       </Route>
-      <Route path="/patient" element={<PatientTop />}>
+      <Route path="/patient" element={<Header />}>
         <Route path="/patient/histories" element={<History />} />
         <Route path="/patient/newhistory" element={<InputPatient />} />
         <Route path="/patient/modifyhistory" element={<InputPatient />} />
       </Route>
-      <Route path="/doctor" element={<DoctorTop />}>
+      <Route path="/doctor" element={<Header />}>
         <Route path="/doctor/histories" element={<History />} />
         <Route path="/doctor/medicine" element={<InputDoctor />} />
       </Route>
